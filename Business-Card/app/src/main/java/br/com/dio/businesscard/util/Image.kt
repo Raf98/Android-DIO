2dc1f796c5a728cc9a264d47aae7282f31e945b4
Raf98 <rafaelrutzsantos@gmail.com>
2021-07-26 04:33:24 -0300

    Solved Image.kt file handling issue on API versions 24+ through 29 by adding FileProvider info on AndroidManifest.xml and creating provider_paths.xml, as well as changing the way the Uri was generated. Now the app seems to be working properly.

--- Business-Card/app/src/main/java/br/com/dio/businesscard/util/Image.kt
+++ Business-Card/app/src/main/java/br/com/dio/businesscard/util/Image.kt
@@ -12,6 +12,8 @@ import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.FileProvider
+import br.com.dio.businesscard.BuildConfig
 import br.com.dio.businesscard.R
 import java.io.File
 import java.io.FileOutputStream
@@ -68,7 +70,9 @@ class Image {
                 // These for devices running on android < Q
                 val imagesDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
                 val image = File(imagesDir, filename)
-                shareIntent(context, Uri.fromFile(image))
+                val imageUri = FileProvider.getUriForFile(context, context.applicationContext.packageName + ".provider", image)
+                //shareIntent(context, Uri.fromFile(image))
+                shareIntent(context, imageUri)
                 fos = FileOutputStream(image)
             }
 
@@ -84,6 +88,7 @@ class Image {
                 putExtra(Intent.EXTRA_STREAM, image)
                 type = "image/jpeg"
             }
+
             context.startActivity(
                 Intent.createChooser(
                     shareIntent,
